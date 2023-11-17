2866519eacafee85249a6aac833d5f7cb287e033
yogesh1000 <42298135+yogesh1000@users.noreply.github.com>
2023-11-16 23:14:27 -0600

    Update Tax.js

--- pages/Tax.js
+++ pages/Tax.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { Alert, View, Text, StyleSheet, Dimensions } from 'react-native';
-import { Input, Header, Icon, Button } from '@rneui/themed';
+import { Input, Icon, Button } from '@rneui/themed';
 import DropDownPicker from 'react-native-dropdown-picker';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Heatmap } from 'react-native-maps';
 import { useNavigation } from "@react-navigation/native";
+import { Header } from '@rneui/themed'; // Import Header from your library
 import SideMenu from "react-native-side-menu";
 
 export default function RateScope() {
@@ -12,32 +13,59 @@ export default function RateScope() {
 
   const [isOpen, setIsOpen] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('autoTax');
-  const [selectedState, setSelectedState] = useState('IL');
+  const [selectedState, setSelectedState] = useState('AL'); // Default state
+  const [taxMatrix, setTaxMatrix] = useState({
+    autoTax: {
+      AL: 5.2,
+      AK: 6.8,
+      IL: 4.5,
+      // Add more states and values as needed
+    },
+    incomeTax: {
+      AL: 3.5,
+      AK: 7.2,
+      IL: 5.8,
+      // Add more states and values as needed
+    },
+  });
+
   const [randomTaxRate, setRandomTaxRate] = useState(null);
 
-  const taxCategories = [
-    { label: 'Auto Tax', value: 'autoTax' },
-    { label: 'Income Tax', value: 'incomeTax' },
-    // Add more tax categories as needed
-  ];
-
-  const usStates = [
-    { label: 'Alabama', value: 'AL', coordinates: { latitude: 32.806671, longitude: -86.791130 } },
-    { label: 'Alaska', value: 'AK', coordinates: { latitude: 61.016977, longitude: -149.243286 } },
-    // Add more states with coordinates
-    { label: 'Illinois', value: 'IL', coordinates: { latitude: 40.633125, longitude: -89.398528 } },
-  ];
-
   const generateRandomTaxRate = () => {
-    // Generate a random tax rate between 1% and 10%
-    const rate = Math.random() * (10 - 1) + 1;
-    setRandomTaxRate(rate.toFixed(2)); // Keep only two decimal places
+    const taxRate = taxMatrix[selectedCategory][selectedState];
+    setRandomTaxRate(taxRate.toFixed(2)); // Keep only two decimal places
   };
 
+  const getHeatmapData = () => {
+    // Convert taxMatrix into an array of {latitude, longitude, weight} for heatmap
+    const heatmapData = Object.entries(taxMatrix[selectedCategory]).map(([state, rate]) => ({
+      latitude: stateCoordinates[state].latitude,
+      longitude: stateCoordinates[state].longitude,
+      weight: rate,
+    }));
+    return heatmapData;
+  };
+
+  const stateCoordinates = {
+    AL: { latitude: 32.806671, longitude: -86.791130 },
+    AK: { latitude: 61.016977, longitude: -149.243286 },
+    IL: { latitude: 40.633125, longitude: -89.398528 },
+    // Add more states and coordinates as needed
+  };
+
+  const menu =
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      {/* Add your menu items here */}
+    </View>;
+
   return (
-    <SideMenu /* ... your existing SideMenu code ... */>
+    <SideMenu menu={menu} isOpen={isOpen}>
       <View style={{ flex: 1, backgroundColor: 'white' }}>
-        <Header /* ... your existing Header code ... */ />
+        <Header
+          leftComponent={<Icon name='menu' size={30} onPress={() => setIsOpen(!isOpen)} />}
+          centerComponent={{ text: 'RateScope', style: { color: '#fff', fontSize: 20 } }}
+          rightComponent={<Icon name='home' onPress={() => navigation.navigate('Login')} size={30} />}
+        />
 
         <View style={styles.container}>
           <Text style={styles.title}>Tax Lookup</Text>
@@ -45,52 +73,66 @@ export default function RateScope() {
           <View style={styles.dropdownContainer}>
             {/* First Dropdown for Tax Category */}
             <DropDownPicker
-            open={isOpen}
-            value={selectedCategory}
-            items={taxCategories}
-            placeholder="Select Tax Category"
-            containerStyle={{ height: 40, width: screenWidth / 2 - 15 }}
-            style={{ backgroundColor: '#fafafa' }}
-            itemStyle={{ justifyContent: 'flex-start' }}
-            dropDownStyle={{ backgroundColor: '#fafafa' ,zindex: 2}}
-            onChangeItem={(item) => {
+              open={isOpen}
+              value={selectedCategory}
+              items={[
+                { label: 'Auto Tax', value: 'autoTax' },
+                { label: 'Income Tax', value: 'incomeTax' },
+                // Add more tax categories as needed
+              ]}
+              placeholder="Select Tax Category"
+              containerStyle={{ height: 40, width: screenWidth / 2 - 15 }}
+              style={{ backgroundColor: '#fafafa' }}
+              itemStyle={{ justifyContent: 'flex-start' }}
+              dropDownStyle={{ backgroundColor: '#fafafa', zIndex: 2 }}
+              onChangeItem={(item) => {
                 setSelectedCategory(item.value);
                 setIsOpen(false); // Close the dropdown after selecting an item
-            }}
+              }}
             />
 
+            {/* Second Dropdown for US States */}
             <DropDownPicker
-            open={isOpen}
-            value={selectedState}
-            items={usStates}
-            placeholder="Select State"
-            containerStyle={{ height: 40, width: screenWidth / 2 - 15 }}
-            style={{ backgroundColor: '#fafafa' }}
-            itemStyle={{ justifyContent: 'flex-start' }}
-            dropDownStyle={{ backgroundColor: '#fafafa' }}
-            onChangeItem={(item) => {
+              open={isOpen}
+              value={selectedState}
+              items={Object.keys(taxMatrix[selectedCategory])}
+              placeholder="Select State"
+              containerStyle={{ height: 40, width: screenWidth / 2 - 15 }}
+              style={{ backgroundColor: '#fafafa' }}
+              itemStyle={{ justifyContent: 'flex-start' }}
+              dropDownStyle={{ backgroundColor: '#fafafa', zIndex: 2 }}
+              onChangeItem={(item) => {
                 setSelectedState(item.value);
                 setIsOpen(false); // Close the dropdown after selecting an item
-            }}
+              }}
             />
-
-            
           </View>
 
-          {/* MapView component */}
+          {/* MapView component with Heatmap */}
           <MapView
             style={{ flex: 1, height: 200 }}
             initialRegion={{
-              latitude: usStates.find((state) => state.value === selectedState).coordinates.latitude,
-              longitude: usStates.find((state) => state.value === selectedState).coordinates.longitude,
+              latitude: stateCoordinates[selectedState].latitude,
+              longitude: stateCoordinates[selectedState].longitude,
               latitudeDelta: 5,
               longitudeDelta: 5,
             }}
           >
             {/* Marker for the selected state */}
             <Marker
-              coordinate={usStates.find((state) => state.value === selectedState).coordinates}
+              coordinate={stateCoordinates[selectedState]}
               title={selectedState}
+            />
+            {/* Heatmap overlay */}
+            <Heatmap
+              points={getHeatmapData()}
+              radius={40}
+              opacity={0.7}
+              gradient={{
+                colors: ["#00FF00", "#FF0000"],
+                startPoints: [0.2, 0.8],
+                colorMapSize: 256,
+              }}
             />
           </MapView>
 
