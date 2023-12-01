9a890797439251537f93a03564664b10d3ad51af
Yuqing Su <yuqings3@illinois.edu>
2023-12-01 16:39:04 -0600

    adding

--- pages/Spending.js
+++ pages/Spending.js
@@ -19,21 +19,69 @@ export default function Spending() {
     const expensesCtx = useContext(ExpensesContext);
     let income = 0;
     let expense = 0;
+    
+    const autoTax = {
+      "None": 0.0,   
+      "AL": 5.2,
+      "AK": 6.8,
+      "AZ": 5.6,
+      "AR": 6.9,
+      "CA": 7.25,
+      "CO": 5.3,
+      "CT": 6.35,
+      "DE": 6.6,
+      "DC": 5.75,
+      "FL": 6.0,
+      "GA": 7.25,
+      "HI": 4.45,
+      "ID": 7.25,
+      "IL": 4.5,
+      "IN": 7.0,
+      "IA": 6.0,
+      "KS": 6.5,
+      "KY": 6.0,
+      "LA": 5.0,
+      "ME": 5.5,
+      "MD": 6.0,
+      "MA": 6.65,
+      "MI": 4.35,
+      "MN": 5.3,
+      "MS": 7.0,
+      "MO": 5.4,
+      "MT": 7.0,
+      "NE": 5.5,
+      "NV": 7.75,
+      "NH": 5.2,
+      "NJ": 6.6,
+      "NM": 5.1,
+      "NY": 4.3,
+      "NC": 5.25,
+      "ND": 5.0,
+      "OH": 5.75,
+      "OK": 5.5,
+      "OR": 0.0,
+      "PA": 6.0,
+      "RI": 7.0,
+      "SC": 6.0,
+      "SD": 4.5,
+      "TN": 7.0,
+      "TX": 6.15,
+      "UT": 4.7,
+      "VT": 6.35,
+      "VA": 4.3,
+      "WA": 6.5,
+      "WV": 6.0,
+      "WI": 5.32,
+      "WY": 5.5,
+    };
     for (let e of expensesCtx.expenses) {
         if (e.type === "expense") {
-            if (e.rate === "AL") {
-                expense += e.amount * 1.052;
-            } else if (e.rate === "AK") {
-                expense += e.amount * 1.068;
-            } else if (e.rate === "IL") {
-                expense += e.amount * 1.045;
-            } else {
-                expense += e.amount;
-            }
+            expense += e.amount*(1+autoTax[e.rate]/100);
         } else {
             income += e.amount;
         }
     }
+
 
     function expensePressHandler() {
         navigation.navigate('ManageExpense');
